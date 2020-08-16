6c569ac6f31f57d496d76fbe34958350c7396342
Andrew59-boop <millerfaabi@gmail.com>
2020-08-16 17:27:28 +0300

    deployment

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 if config('MODE')=="dev":
    DATABASES = {
        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
            'NAME': config('DB_NAME'),
            'USER': config('DB_USER'),
            'PASSWORD': config('DB_PASSWORD'),
